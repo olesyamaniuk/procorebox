9fdc11e151ece38b81640f392b8e1b1c36217e85
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2025-10-07 16:21:23 +0300

    2

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
-  const navListRef = document.querySelector(".i-k-navigation-list");
-  const headerRef = document.querySelector(".i-k-header");
-  const navigationRef = document.querySelector(".i-k-navigation");
+  const navListRef = document.querySelector(".i-l-navigation-list");
+  const headerRef = document.querySelector(".i-l-header");
+  const navigationRef = document.querySelector(".i-l-navigation");
 
   menuBtnRef.addEventListener("click", () => {
     menuBtnRef.classList.toggle("is-open");
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     navigationRef.classList.toggle("is-open");
   });
 
-  document.querySelectorAll(".i-k-navigation-link").forEach((n) =>
+  document.querySelectorAll(".i-l-navigation-link").forEach((n) =>
     n.addEventListener("click", () => {
       navListRef.classList.remove("is-open");
       menuBtnRef.classList.remove("is-open");
